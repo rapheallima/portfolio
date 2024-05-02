67a3043cbc9cc243c870a7cd81f657cb86bb308a
Raphael Lima <raphaellima365@gmail.com>
2024-05-02 11:03:58 -0300

    criando a seção de projetos e o componente de projeto

---
+++ src/components/Projeto/index.tsx
@@ -0,0 +1,14 @@
+import Paragrafo from '../Paragrafo'
+import Titulo from '../Titulo'
+
+import { Card, LinkBotao } from './styles'
+
+const Projeto = () => (
+  <Card>
+    <Titulo fontSize={0}>Projeto Lista de Tarefas</Titulo>
+    <Paragrafo tipo="secundario">Lista de tarefas feita com VueJS</Paragrafo>
+    <LinkBotao>Visualizar</LinkBotao>
+  </Card>
+)
+
+export default Projeto

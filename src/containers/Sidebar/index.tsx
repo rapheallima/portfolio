3f222579edbb7cb239d08547560aedaf8fc8fe01
Raphael Lima <raphaellima365@gmail.com>
2024-05-03 09:38:57 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/rapheallima.png" alt="" />
-    <Titulo fontSize={20}>Raphael Lima</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={19}>Raphael Lima</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        rapheallima
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Full Stack Java
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
